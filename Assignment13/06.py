689a36356b1af6e90ce1d550f32e73b71036c1ca
Prasad Wankhade <prasadw350@gmail.com>
2026-07-01 14:55:57 +0530

    Vector programs

---
+++ Assignment13/06.py
@@ -0,0 +1,18 @@
+# Import NumPy 
+import numpy as np
+# Create vectors
+v1 = np.array([2, 4, 6, 8])
+v2 = np.array([1, 3, 5, 7])
+# Addition
+print("Addition:")
+print(v1 + v2)
+# Subtraction
+print("\nSubtraction:")
+print(v1 - v2)
+# Element-wise multiplication
+print("\nElement-wise Multiplication:")
+print(v1 * v2)
+# Dot product
+print("\nDot Product:")
+print(np.dot(v1, v2))
+
