922d99be98cef90f2dc43cf6456c2ce7fae22e8e
Prasad Wankhade <prasadw350@gmail.com>
2026-07-01 14:12:03 +0530

    zeros and ones program

---
+++ Assignment13/02.py
@@ -0,0 +1,15 @@
+# Import NumPy
+import numpy as np
+# 1D array of zeros
+array1 = np.zeros(8)
+# 4x4 array of ones
+array2 = np.ones((4, 4))
+# 3x3 matrix of zeros
+array3 = np.zeros((3, 3))
+# Display arrays
+print("1D Array of Zeros:")
+print(array1)
+print("\n4x4 Array of Ones:")
+print(array2)
+print("\n3x3 Matrix of Zeros:")
+print(array3)
