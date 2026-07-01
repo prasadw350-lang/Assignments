5211196757a24f975da4ff927109703839085baf
Prasad Wankhade <prasadw350@gmail.com>
2026-07-01 14:49:34 +0530

    linspace program

---
+++ Assignment13/04.py
@@ -0,0 +1,15 @@
+# Import NumPy
+import numpy as np
+# a) 10 equally spaced numbers between 0 and 5
+array1 = np.linspace(0, 5, 10)
+# b) 15 equally spaced numbers between -10 and 10
+array2 = np.linspace(-10, 10, 15)
+
+# Display arrays
+print("10 Equally Spaced Numbers:")
+print(array1)
+print("Length:", len(array1))
+print("\n15 Equally Spaced Numbers:")
+print(array2)
+print("Length:", len(array2))
+
