814212c4ac27cbd97c659cf02f2fe0e4cb4a0159
Prasad Wankhade <prasadw350@gmail.com>
2026-07-01 14:14:22 +0530

    arrange program

---
+++ Assignment13/03.py
@@ -0,0 +1,17 @@
+# Import NumPy
+import numpy as np
+# a) Numbers from 0 to 20 (step 1).
+array1 = np.arange(0, 21, 1)
+# b) Even numbers from 10 to 50.
+array2 = np.arange(10, 51, 2)
+# c) Numbers from 5 to 100 with step of 5.
+array3 = np.arange(5, 101, 5)
+
+# Display arrays
+print("Numbers from 0 to 20:")
+print(array1)
+print("\nEven Numbers from 10 to 50:")
+print(array2)
+print("\nNumbers from 5 to 100 (Step 5):")
+print(array3)
+
