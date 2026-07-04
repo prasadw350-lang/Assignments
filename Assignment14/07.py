fe89f8028d07bd1b367904334fa70d404ced47f2
Prasad Wankhade <prasadw350@gmail.com>
2026-07-04 12:52:44 +0530

    Arithmetic Operators

---
+++ Assignment14/07.py
@@ -0,0 +1,18 @@
+# Import NumPy
+import numpy as np
+# Create arrays
+a = np.array([10,20,30,40])
+b = np.array([1,2,3,4])
+print("Addition:")
+print(a + b)
+print("\nSubtraction:")
+print(a - b)
+print("\nMultiplication:")
+print(a * b)
+print("\nDivision:")
+print(a / b)
+print("\nPower:")
+print(a ** b)
+print("\nModulo:")
+print(a % b)
+
